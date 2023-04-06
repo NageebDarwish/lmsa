73d0dda1a5117dfebea9e3ece01f8d59320efee0
unknown <bravotech0sa@gmail.com>
2023-04-06 15:06:11 +0300

    update

--- src/Context/RequireAuth.js
+++ src/Context/RequireAuth.js
@@ -1,13 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { User } from "./UserContext";
+import { baseUrl } from "./../Api/Api";
 
 export default function AuthRequire() {
   const userLogin = useContext(User);
+  const [userObject, setUserObject] = useState("");
+
+  useEffect(() => {
+    fetch(`${baseUrl}/user`, {
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + String(userLogin.auth.Token),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setUserObject(data));
+  }, []);
 
   const location = useLocation();
 
-  return userLogin?.auth?.Token ? (
+  return userObject ? (
     <Outlet />
   ) : (
     <Navigate state={{ from: location }} replace to="/login" />
